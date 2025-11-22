6a7ce2a03486fd9ce61a2070d05952685f36ec5d
Luisa Gomez <89656628+4591526@users.noreply.github.com>
2025-11-22 16:44:42 -0500

    Update PC4.py

--- PC4.py
+++ PC4.py
@@ -45,7 +45,7 @@ pagina_seleccionada = st.sidebar.selectbox('Selecciona la sección que deseas ve
 if pagina_seleccionada == 'Inicio':
 
     # La función st.markdown permite centrar y agrandar la letra del título de la web en Streamlit.
-    st.markdown("<h1 style='text-align: center;'>Aquí ustedes deben editar</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Blog</h1>", unsafe_allow_html=True)
 
     # <h1 style='text-align: center;'>Nombre de tu blog</h1>: Esto es una cadena de código HTML. 
     # La etiqueta <h1> se utiliza para el encabezado principal de una página web, y 
@@ -246,4 +246,5 @@ else:
 
         # Mostrar el mapa en Streamlit
         #st_folium(mapa_cusco, width=700, height=500)
+
     
